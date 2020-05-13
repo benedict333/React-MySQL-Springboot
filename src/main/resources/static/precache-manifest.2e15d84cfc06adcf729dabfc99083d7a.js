75c9f3f514e99b55f61407483dad8dd62052b363
Bob <bobmarley9717@gmail.com>
2020-05-13 16:56:55 -0500

    react build

---
+++ src/main/resources/static/precache-manifest.2e15d84cfc06adcf729dabfc99083d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c16b765c5e3df56d0ff024144ccbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0a9cabb9339a49f8b0",
+    "url": "/static/css/main.0b026e67.chunk.css"
+  },
+  {
+    "revision": "efbb390b297defe84242",
+    "url": "/static/js/2.121329e5.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.121329e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0a9cabb9339a49f8b0",
+    "url": "/static/js/main.dc3d5fd2.chunk.js"
+  },
+  {
+    "revision": "2a39ad781710883b1e84",
+    "url": "/static/js/runtime-main.9ad9ec7f.js"
+  }
+]);
